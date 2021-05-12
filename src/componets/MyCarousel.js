1ebd9fa2c13f01ddc8588f23f3a8188001f69393
meggie keje <alexokoro342@gmail.com>
2021-05-12 13:52:34 +0100

    update

--- src/componets/MyCarousel.js
+++ src/componets/MyCarousel.js
@@ -1,27 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import Carousel from 'react-native-snap-carousel';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import HomeScreen from './home';
+import img1 from '../img/img1.jpg'
+import img2 from '../img/img2.jpg'
+import img3 from '../img/img3.jpg'
 
-const MyCarousel = ({entries}) => {
-    _renderItem = ({item, index}) => {
-        return (
-            <View style={styles.slide}>
-                <Text style={styles.title}>{ item.title }</Text>
-            </View>
-        );
+const slides = [
+    {
+      key: 1,
+      title: 'Title 1',
+      text: 'Description.\nSay something cool',
+      image: img1,
+      backgroundColor: '#59b2ab',
+    },
+    {
+      key: 2,
+      title: 'Title 2',
+      text: 'Other cool stuff',
+      image: img2,
+      backgroundColor: '#febe29',
+    },
+    {
+      key: 3,
+      title: 'Rocket guy',
+      text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+      image: img3,
+      backgroundColor: '#22bcb5',
     }
+  ];
 
-    return (
-        <Carousel
-              ref={(c) => { this._carousel = c; }}
-              data={entries}
-              renderItem={this._renderItem}
-              sliderWidth={sliderWidth}
-              itemWidth={itemWidth}
-            />
-    )
-}
+  export default class App extends React.Component {
+      constructor(props) {
+          super(props)
+          this.state = {
+            showRealApp: false
+          }
 
-export default MyCarousel
+      }
+    _renderItem = ({ item }) => {
+      return (
+        <View style={styles.slide}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Image source={item.image} />
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+      );
+    }
+    _onDone = () => {
+      // User finished the introduction. Show real app through
+      // navigation or simply by controlling state
+      this.setState({ showRealApp: true });
+    }
+    render() {
+      if (this.state.showRealApp) {
+        return <App />;
+      } else {
+        return <AppIntroSlider renderItem={this._renderItem} data={slides} onDone={this._onDone}/>;
+      }
+    }
+  }
 
-const styles = StyleSheet.create({})
+  const styles = StyleSheet.create({
+
+  })
